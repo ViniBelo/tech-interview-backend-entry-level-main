d8ea479349fa6b3fa41b4d52bb1e270a6e681749
ViniBelo <vinialvesbelo@outlook.com>
2026-04-20 22:05:11 -0300

    Cart logics to pass through initial tests

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,15 @@
 class Cart < ApplicationRecord
-  validates_numericality_of :total_price, greater_than_or_equal_to: 0
+  validates :total_price, numericality: { greater_than_or_equal_to: 0 }
 
-  # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+  def mark_as_abandoned = update(abandoned: true)
+
+  def abandoned_for_a_week_or_more?
+    return false unless abandoned?
+
+    last_interaction_at.before?(7.days.ago)
+  end
+
+  def remove_if_abandoned
+    destroy if abandoned_for_a_week_or_more?
+  end
 end
